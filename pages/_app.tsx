e8dff42a9c5fef1fa2aba9b68bd9c4f48a9cf811
m-arsal-jadoon <marsaljadoon7@gmail.com>
2026-04-12 17:28:55 +0500

    fix: wrap legacy pages router _app.tsx with ThemeProvider to fix Section page dark mode toggle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import { ThemeProvider } from '../components/ThemeProvider'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout breadcrumbs={{ 
-      code: pageProps.codeData, 
-      category: pageProps.category, 
-      chapter: pageProps.chapter,
-      section: pageProps.sectionId ? { id: pageProps.sectionId, chapter_title: pageProps.chapterTitle } : undefined
-    }}>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Layout breadcrumbs={{ 
+        code: pageProps.codeData, 
+        category: pageProps.category, 
+        chapter: pageProps.chapter,
+        section: pageProps.sectionId ? { id: pageProps.sectionId, chapter_title: pageProps.chapterTitle } : undefined
+      }}>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   )
 }
 
