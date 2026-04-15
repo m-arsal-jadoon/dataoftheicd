c068f901c3f63f323c46a458a63a15e0ab9b3c83
m-arsal-jadoon <marsaljadoon7@gmail.com>
2026-04-16 00:42:02 +0500

    added edge runtime config

--- app/icd10cm/2026/chapter/[slug]/page.tsx
+++ app/icd10cm/2026/chapter/[slug]/page.tsx
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import Link from 'next/link';
 import { getAllChapters, getSubSectionsByChapter } from '../../../../../lib/data-engine/db';
 import Layout from '../../../../../components/Layout';
--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs';
 import path from 'path';
